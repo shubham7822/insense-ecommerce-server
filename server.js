03f8148068a91fa816f89a2fca70a1233f00efe6
shubham mansute <shubhammansute7822@gmail.com>
2023-11-01 08:28:04 +0530

    allowing all origins

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-    origin:"https://shivkrupa.onrender.com"
-}))
+app.use(cors())
 app.use(express.json());
 
 app.get("/",(req,res) => {
